07043f4fcb482686d4485cf86f7ecb3c4bff93cb
vijayskk <vijayskk007@gmail.com>
2022-06-30 16:28:43 +0530

    Camera Lens fixed

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ console.log("The Model used here : Ancient Corinth - Peirene Fountain fuzzelhjb 
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75 , window.innerWidth/window.innerHeight , 0.001 , 1000000)
+const camera = new THREE.PerspectiveCamera(60 , window.innerWidth/window.innerHeight , 0.001 , 1000000)
 
 const renderer = new THREE.WebGLRenderer({
   canvas:document.querySelector('#bg'),
